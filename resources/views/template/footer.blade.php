1542907c1c2544066b0327614fe6f1a4e51dc64a
brianwibowo <sultanapri86@gmail.com>
2025-05-05 13:44:50 +0700

    yeni inka

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="container-fluid d-flex justify-content-between">
+    <nav class="pull-left">
+        <ul class="nav">
+            <li class="nav-item">
+                Developed by 
+                <a target="_blank" href="http://www.themekita.com">
+                    Sultan Studio
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div>
+        Theme by
+        <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
+    </div>
+</div>
